1fb99a515c0fc14395950e7c5da1b6fa75fa85c2
XuanTing Lin <ryan027151@gmail.com>
2025-07-08 08:41:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
+from datetime import datetime
+import matplotlib.pyplot as plt
+import seaborn as sns
 import yfinance as yf
 
-def search(x):
-   ticker = x
-   data = yf.download(x, start="2025-01-01", end="2025-07-01")
+end_date = datetime(year=2025, month=7, day=7)
+start_date = datetime(year=2025, month=1, day=1)
 
-   start = data.head()
-   end = data.tail()
-   return start, end
-
-print(search("NVDA"))
+ticker = 'NVDA'
+df_single = yf.download(
+    tickers=ticker,
+    start=start_date,
+    end=end_date,
+    interval="1d",
+    group_by="ticker",
+    auto_adjust=True,
+    progress=False
+)
+df_single.columns
