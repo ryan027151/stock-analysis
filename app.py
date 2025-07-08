f8548be01bb919923bdaa21a8f435589a28fed4b
XuanTing Lin <ryan027151@gmail.com>
2025-07-08 09:07:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,16 @@ import yfinance as yf
 end_date = datetime(year=2025, month=7, day=7)
 start_date = datetime(year=2025, month=1, day=1)
 
-ticker = 'NVDA'
-df_single = yf.download(
-    tickers=ticker,
-    start=start_date,
-    end=end_date,
-    interval="1d",
-    group_by="ticker",
-    auto_adjust=True,
-    progress=False
-)
-df_single.columns
+def stock(ticker):
+   df_single = yf.download(
+      tickers=ticker,
+      start=start_date,
+      end=end_date,
+      interval="1d",
+      group_by="ticker",
+      auto_adjust=True,
+      progress=False
+   )
+   return(df_single.head(),df_single.tail())  
+
+print(stock("NVDA"))      # See the first few rows
